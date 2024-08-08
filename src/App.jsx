3b8b56d98aea06dda50bd4180a8c599b303e0524
Bastien Ricard <bastien.ricard-dev@outlook.com>
2024-08-08 16:04:32 +0200

    Add initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-function App() {
- 
-  return (
+import loader from './assets/loader.svg';
 
-      <main>
-      
-      </main>
-  
+function App() {
+  return (
+    <main>
+      <div className="loader-container">
+        <img src={loader} alt="loader icon" />
+      </div>
+      <p className="city-name">Paris</p>
+      <p className="country-name">France</p>
+      <p className="temperature">17°</p>
+      <div className="info-icon-container">
+        <img src="/icons/01d.svg" alt="weather icon" />
+      </div>
+    </main>
   );
 }
 
